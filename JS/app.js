ebfb46bbe26de672f52ef75e0b90e2672e7f3a25
olashlool <olashlool94@gmail.com>
2022-02-19 03:06:29 +0200

    Task event

--- JS/app.js
+++ JS/app.js
@@ -11,13 +11,13 @@ function HrMangment (id , fullName, department, level, image ){
 }
 
 
-const employee1 = new HrMangment(1000,"Ghazi Samer",["Administration"],["Senior"],`./images/boy.jpg`);
-const employee2 = new HrMangment(1001,"Lana Ali",["Finance"],["Senior"],`./images/girl.jpg`);
-const employee3 = new HrMangment(1002,"Tamara Ayoub",["Marketing"],["Senior"],`./images/girl.jpg`);
-const employee4 = new HrMangment(1003,"Safi Walid",["Administration"],["Mid-Senior"],`./images/boy.jpg`);
-const employee5 = new HrMangment(1004,"Omar Zaid",["Development"],["Senior"],`./images/boy.jpg`);
-const employee6 = new HrMangment(1005,"Rana Saleh",["Development"],["Junior"],`./images/girl.jpg`);
-const employee7 = new HrMangment(1006,"Hadi Ahmad",["Finance"],["Mid-Senior"],`./images/boy.jpg`);
+const employee1 = new HrMangment(1000,"Ghazi Samer",["Administration"],["Senior"],`../images/boy.png`);
+const employee2 = new HrMangment(1001,"Lana Ali",["Finance"],["Senior"],`./images/girl.png`);
+const employee3 = new HrMangment(1002,"Tamara Ayoub",["Marketing"],["Senior"],`./images/girl.png`);
+const employee4 = new HrMangment(1003,"Safi Walid",["Administration"],["Mid-Senior"],`./images/boy.png`);
+const employee5 = new HrMangment(1004,"Omar Zaid",["Development"],["Senior"],`./images/boy.png`);
+const employee6 = new HrMangment(1005,"Rana Saleh",["Development"],["Junior"],`./images/girl.png`);
+const employee7 = new HrMangment(1006,"Hadi Ahmad",["Finance"],["Mid-Senior"],`./images/boy.png`);
 
 
 function randomNum(min, max) {
@@ -38,12 +38,102 @@ HrMangment.prototype.calculatingSalary = function (){
     this.salary = this.salary - this.salary * 0.075;
 }
 
-HrMangment.prototype.render = function(){
-    document.write(`<p>My name is ${this.fullName} i work in this company and my salary is : $${this.salary}</p>`)
-}
-
 for (let i =0 ; i< allEmployee.length; i++){
     allEmployee[i].calculatingSalary();
+}
+
+
+var getRandomId = function () {
+    return Math.floor(Math.random() * (9000- 1000)) + 1000;
+}
+
+let employeeForm =document.getElementById("employeeForm");
+employeeForm.addEventListener( "submit",ADD);
+
+function ADD(event){
+    event.preventDefault();
+    let fullName =event.target.fullName.value;
+    let department =event.target.department.value;
+    let level =event.target.level.value;
+    let image =event.target.image.value;
+
+    let newEmployee = new HrMangment(getRandomId(), fullName,department,level,image);
+    newEmployee.calculatingSalary();
+    newEmployee.rendernew();
+}
+
+let emplooyesDiv =document.getElementById("emplooyes");
+let recentlyEmployee =document.getElementById("recentlyEmployee");
+let administrationDepartment =document.getElementById("administrationDepartment");
+let marketingDepartment =document.getElementById("marketingDepartment");
+let developmentDepartment =document.getElementById("developmentDepartment");
+let financeDepartment =document.getElementById("financeDepartment");
+
+HrMangment.prototype.rendernew = function(){
+    let heading=document.createElement("p");
+        heading.textContent="Name: "+this.fullName +", ID: "+this.id;
+    let heading1=document.createElement("p");
+        heading1.textContent="Depaertment: "+this.department +", Level: "+this.level;
+    let heading2=document.createElement("p");
+        heading2.textContent="Salary: "+this.salary;
+    
+    let images=document.createElement("img");
+        images.setAttribute("src",this.image);
+        images.setAttribute("width","150");
+        images.setAttribute("height","150");
+
+            emplooyesDiv.appendChild(recentlyEmployee);
+            recentlyEmployee.appendChild(images);
+            recentlyEmployee.appendChild(heading);
+            recentlyEmployee.appendChild(heading1);
+            recentlyEmployee.appendChild(heading2);
+}
+HrMangment.prototype.render = function(){
+    let heading=document.createElement("p");
+        heading.textContent="Name: "+this.fullName +", ID: "+this.id;
+    let heading1=document.createElement("p");
+        heading1.textContent="Depaertment: "+this.department +", Level: "+this.level;
+    let heading2=document.createElement("p");
+        heading2.textContent="Salary: "+this.salary;
+    
+    let images=document.createElement("img");
+        images.setAttribute("src",this.image);
+        images.setAttribute("width","150");
+        images.setAttribute("height","150");
+
+        
+
+        if (this.department == "Administration"){
+            emplooyesDiv.appendChild(administrationDepartment);
+            administrationDepartment.appendChild(images);
+            administrationDepartment.appendChild(heading);
+            administrationDepartment.appendChild(heading1)
+            administrationDepartment.appendChild(heading2)
+        } else if (this.department == "Marketing"){
+            emplooyesDiv.appendChild(marketingDepartment);
+            marketingDepartment.appendChild(images);
+            marketingDepartment.appendChild(heading);
+            marketingDepartment.appendChild(heading1)
+            marketingDepartment.appendChild(heading2)
+        }else if (this.department == "Development"){
+            emplooyesDiv.appendChild(developmentDepartment);
+            developmentDepartment.appendChild(images);
+            developmentDepartment.appendChild(heading);
+            developmentDepartment.appendChild(heading1)
+            developmentDepartment.appendChild(heading2)
+        }else if (this.department == "Finance"){
+            emplooyesDiv.appendChild(financeDepartment);
+            financeDepartment.appendChild(images);
+            financeDepartment.appendChild(heading);
+            financeDepartment.appendChild(heading1)
+            financeDepartment.appendChild(heading2)
+        }else{
+            emplooyesDiv.appendChild(recentlyEmployee);
+            recentlyEmployee.appendChild(imageslist);
+            recentlyEmployee.appendChild(heading);
+            recentlyEmployee.appendChild(heading1)
+            recentlyEmployee.appendChild(heading2)
+        }
 }
 
 for (let i =0 ; i< allEmployee.length; i++){
